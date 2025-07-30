0f191966edcedbdaea45b2d975b020fddb6eb02e
Aaditya Bhardwaj <113894836+imaadi07@users.noreply.github.com>
2025-07-30 08:41:18 +0530

    backend fixes

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ database();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
--- backend/middleware/passport.js
+++ backend/middleware/passport.js
@@ -33,7 +33,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL: `${process.env.BASE_URL}/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
